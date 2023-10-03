487b62fefd145440c9218a9d187911076294357c
Demetre-Panjakidze <demetrepanjakidze1@gmail.com>
2023-10-03 12:37:03 +0400

    Fetched authenticated users information, stored it

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -3,8 +3,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 
-final _firebase = FirebaseAuth.instance;
-User? _user = _firebase.currentUser;
+final FirebaseAuth _firebase = FirebaseAuth.instance;
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
 
 class HomepageScreen extends StatefulWidget {
   const HomepageScreen({super.key});
@@ -14,77 +14,133 @@ class HomepageScreen extends StatefulWidget {
 }
 
 class _HomepageScreenState extends State<HomepageScreen> {
+  User? _user;
+  DocumentReference? _userDocRef;
+  Map<String, dynamic>? _userData;
+
+  @override
+  void initState() {
+    super.initState();
+    _user = _firebase.currentUser;
+    _userDocRef = _firestore.collection('users').doc(_user!.uid);
+
+    fetchUserData();
+  }
+
+  Future<void> fetchUserData() async {
+    try {
+      if (_userDocRef != null) {
+        final DocumentSnapshot userSnapshot = await _userDocRef!.get();
+        _userData = userSnapshot.data() as Map<String, dynamic>;
+        setState(() {});
+      }
+    } catch (error) {
+      print('Error fetching user data: $error');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      drawer: Drawer(
-        child: ListView(
-          padding: EdgeInsets.zero,
-          children: [
-            const DrawerHeader(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-              ),
-              child: Text(
-                'Navigation center',
-                style: TextStyle(
-                  fontSize: 26,
-                  color: Colors.white,
+    if (_userDocRef != null && _user != null && _userData != null) {
+      return Scaffold(
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: Colors.blue,
+                ),
+                child: Container(
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      backgroundImage: NetworkImage(_userData!["photoURL"]),
+                    ),
+                    title: Text(
+                      _userData!["displayName"],
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
                 ),
               ),
+              ListTile(
+                title: const Text('Dashboard'),
+                onTap: () {},
+              ),
+              if (_userData!["role"] == 'member')
+                ListTile(
+                  title: const Text('Doctors'),
+                  onTap: () {},
+                ),
+              if (_userData!["role"] == 'practitioner')
+                ListTile(
+                  title: const Text('Requests'),
+                  onTap: () {},
+                ),
+              ListTile(
+                title: const Text('Upcoming consultations'),
+                onTap: () {},
+              ),
+              if (_userData!["role"] == 'member')
+                ListTile(
+                  title: const Text('Health records'),
+                  onTap: () {},
+                ),
+              if (_userData!["role"] == 'practitioner')
+                ListTile(
+                  title: const Text('Population'),
+                  onTap: () {},
+                ),
+            ],
+          ),
+        ),
+        appBar: AppBar(
+          title: const Text('Medbooker'),
+          actions: <Widget>[
+            OutlinedButton(
+              child: const Text(
+                "Log out",
+                style: TextStyle(
+                  color: Colors.redAccent,
+                ),
+              ),
+              onPressed: () async {
+                await Future.delayed(
+                  const Duration(
+                    milliseconds: 300,
+                  ),
+                );
+                _firebase.signOut();
+              },
             ),
-            ListTile(
-              title: const Text('Dashboard'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Doctors'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Requests'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Upcoming consultations'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Health records'),
-              onTap: () {},
-            ),
-            ListTile(
-              title: const Text('Population'),
-              onTap: () {},
-            ),
+            const SizedBox(
+              width: 10,
+            )
           ],
         ),
-      ),
-      appBar: AppBar(
-        title: const Text('Medbooker'),
-        actions: <Widget>[
-          OutlinedButton(
-            child: const Text(
-              "Log out",
-              style: TextStyle(
-                color: Colors.redAccent,
+        body: Center(
+          child: Column(
+            children: [
+              OutlinedButton(
+                onPressed: () {
+                  print(_userData);
+                },
+                child: const Text('Print user info'),
               ),
-            ),
-            onPressed: () async {
-              await Future.delayed(
-                const Duration(
-                  milliseconds: 300,
-                ),
-              );
-              // _firebase.signOut();
-              print(_user);
-            },
+            ],
           ),
-          const SizedBox(
-            width: 10,
-          )
+        ),
+      );
+    } else {
+      return const Column(
+        children: [
+          CircularProgressIndicator(),
         ],
-      ),
-    );
+      );
+    }
   }
 }
